c59ddc1ba2f5c83848d864ea069e90d6cd417a1c
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2023-01-21 11:20:54 -0500

    Añade funcionalidad de convertir temperatura

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+    let newtemp=0;
+    newtemp=(temperature-32)*(5/9);
+    newtemp=Math.round(newtemp*10)/10;
+    return newtemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+    let newtemp=0;
+    newtemp=(temperature*(9/5))+32;
+    newtemp=Math.round(newtemp*10)/10;
+    return newtemp;
 };
 
 // Do not edit below this line
