183e7aa1550cd311f32a7d84e71494321df3b58f
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2023-01-21 10:15:09 -0500

    Añade funcionalidad para eliminar items de array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array,...toRemove) {
+    for(const item of toRemove){
+        let index=array.indexOf(item);
+        if(index<0)continue;
+        array.splice(index,1);
+    }
+    return array;
 };
 
 // Do not edit below this line
