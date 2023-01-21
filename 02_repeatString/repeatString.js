3a9ea751848f741970b65135ab553f8173be4221
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2023-01-21 08:19:37 -0500

    Añade codigo a la funcion para pasar tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    let finalString='';
+    for(let i=0;i<num;i++){
+        finalString+=string;
+    }
+    if(num<0)finalString='ERROR';
+    return finalString;
 };
 
 // Do not edit below this line
